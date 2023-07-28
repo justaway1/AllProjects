7c4ffdeb36e8a21900ab11c70110f8f8465b3f82
Stanislav Angelov <stannyweb@gmail.com>
2023-07-28 18:37:39 +0300

    Added Some JS

---
+++ main.js
@@ -0,0 +1,6 @@
+const accessKey = 'ignoBT9iZvBzX3rMfHHu2E1q4HfTxDw8xDpKN4DWzyQ'
+
+const inputEl = document.querySelector('#image_search_app_search_images')
+const searchBtn = document.querySelector('#search_image_button')
+const imagesContainer = document.querySelector('#image_search_app_results')
+const showMoreBtn = document.querySelector('#button_show_more')
